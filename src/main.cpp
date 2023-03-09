bb24b15c43b9364f4465878e2dab672bcad9a8b8
rd316 <rd316.gortor20@gmail.com>
2023-03-09 15:22:31 +0300

    improve the debugger and addapt the main function to new functionality, launch tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #define SDL_MAIN_HANDLED
-#include "gameboyemulator.h"
 #include "imgui.h"
+#include "gameboyemulator.h"
+#include "gbtest.h"
 #include "backends/imgui_impl_sdl.h"
 #include "backends/imgui_impl_sdlrenderer.h"
 
@@ -10,7 +11,31 @@
 #include <fstream>
 #include <iomanip>
 
-std::shared_ptr<uint8_t*> load_file(const std::string &path, size_t *size)
+typedef struct BREAKPOINT {
+    uint16_t address;
+    bool enabled;
+} Breakpoint;
+
+std::vector<Breakpoint> breakpoints = {
+    {0xc7af, true},
+    {0xc7b0, true},
+    {0xc7b1, true},
+    {0xc7b2, true},
+    {0x0100, true},
+};
+
+bool use_breakpoints = false;
+bool cycling_enabled = true;
+bool breakpoint_reached = false;
+
+bool output_instr = true;
+bool testing_mode = true;
+
+int cycles_per_frame = 100000;
+
+GbE *emu;
+
+uint8_t* load_file(const std::string &path, size_t *size)
 {
     std::ifstream input(path, std::ios::binary);
 
@@ -22,36 +47,43 @@ std::shared_ptr<uint8_t*> load_file(const std::string &path, size_t *size)
     input.close();
 
     *size = buffer.size();
-    uint8_t *data = new uint8_t[buffer.size()];
+    uint8_t *ptr = (uint8_t*) malloc(*size);
+    std::copy(buffer.begin(), buffer.end(), ptr);
 
-    std::copy(buffer.begin(), buffer.end(), data);
-
-    return std::make_shared<uint8_t*>(data);
+    return ptr;
 }
 
-void debuggerRegisters(GbE *emu)
+void debugger_main()
 {
-    if (ImGui::BeginTable("Registers", 7)) {
+    ImGui::Begin("Registers");
+    ImGui::Text("PC: %#04x\n ", emu->get_PC());
+
+    if (ImGui::BeginTable("GbAccess::Registers", 7)) {
         ImGui::TableNextRow();
         char names[] = {'A', 'B', 'C', 'D', 'E', 'H', 'L'};
         for (char c : names) {
             ImGui::TableNextColumn();
             ImGui::Text("%c", c);
         }
-        Reg8 regs[] = {Reg8::A, Reg8::B, Reg8::C, Reg8::D, Reg8::E, Reg8::H, Reg8::L};
+        GbAccess::Reg8 regs[] = {
+            GbAccess::Reg8::A,
+            GbAccess::Reg8::B,
+            GbAccess::Reg8::C,
+            GbAccess::Reg8::D,
+            GbAccess::Reg8::E,
+            GbAccess::Reg8::H,
+            GbAccess::Reg8::L
+        };
 
         ImGui::TableNextRow();
-        for (Reg8 r : regs) {
+        for (GbAccess::Reg8 r : regs) {
             ImGui::TableNextColumn();
             ImGui::Text("%02X", emu->read_register8(r));
         }
 
         ImGui::EndTable();
     }
-}
 
-void debuggerFlags(const GbE *emu)
-{
     if (ImGui::BeginTable("Flags", 4)) {
         ImGui::TableNextRow();
         char names[] = {'Z', 'N', 'H', 'C'};
@@ -62,16 +94,99 @@ void debuggerFlags(const GbE *emu)
 
         ImGui::TableNextRow();
         ImGui::TableNextColumn();
-        ImGui::Text("%d", emu->get_Z() ? 1 : 0);
+        ImGui::Text("%d", emu->get_flag(GbAccess::Flag::Z) ? 1 : 0);
         ImGui::TableNextColumn();
-        ImGui::Text("%d", emu->get_N() ? 1 : 0);
+        ImGui::Text("%d", emu->get_flag(GbAccess::Flag::N) ? 1 : 0);
         ImGui::TableNextColumn();
-        ImGui::Text("%d", emu->get_H() ? 1 : 0);
+        ImGui::Text("%d", emu->get_flag(GbAccess::Flag::H) ? 1 : 0);
         ImGui::TableNextColumn();
-        ImGui::Text("%d", emu->get_C() ? 1 : 0);
+        ImGui::Text("%d", emu->get_flag(GbAccess::Flag::C) ? 1 : 0);
 
         ImGui::EndTable();
     }
+
+    if (ImGui::Button("Cycle")) {
+        uint16_t PC = emu->get_PC();
+
+        emu->execute();
+
+        if (output_instr) {
+            std::cout << std::hex << std::setfill('0')
+                      << std::setw(4) << PC
+                      << std::dec << "\t"
+                      << emu->get_inst() << "\t"
+                      << emu->get_arg1() << "\t"
+                      << emu->get_arg2() << std::endl;
+        }
+
+        if (emu->isHalted()) {
+            std::cout << "Received HALT" << std::endl;
+        } else if (emu->isStopped()) {
+            std::cout << "Received STOP" << std::endl;
+        }
+    }
+
+    if (cycling_enabled && !breakpoint_reached) {
+        if (ImGui::Button("Pause")) {
+            cycling_enabled = false;
+        }
+    } else {
+        if (ImGui::Button("Resume")) {
+            cycling_enabled = true;
+            breakpoint_reached = false;
+        }
+    }
+
+    if (ImGui::Button("Print SPI")) {
+        std::cout << "SPI buffer data:\n"
+                  << emu->get_spi_buffer_data().data() << std::endl
+                  << "SPI buffer data end" << std::endl;
+    }
+
+    ImGui::End();
+}
+
+void debugger_breakpoints()
+{
+    ImGui::Begin("Breakpoints");
+
+    ImGui::Checkbox("Breakpoints active", &use_breakpoints);
+
+    if (ImGui::BeginTable("Breakpoints", 2)) {
+        ImGui::TableNextRow();
+        ImGui::TableNextColumn();
+        ImGui::Text("Address");
+        ImGui::TableNextColumn();
+        ImGui::Text("Enabled");
+
+        for (auto i = breakpoints.begin(); i != breakpoints.end(); i++) {
+            ImGui::TableNextRow();
+            ImGui::TableNextColumn();
+            ImGui::Text("%#04x", i->address);
+
+            ImGui::TableNextColumn();
+
+            auto label = std::to_string(ImGui::TableGetRowIndex());
+            ImGui::Checkbox(label.c_str(), &(i->enabled));
+        }
+
+        ImGui::EndTable();
+    }
+    ImGui::End();
+}
+
+void debugger_fps_counter()
+{
+    ImGui::NewFrame();
+    ImGui::Begin("FPS");
+    ImGui::Text("%.1f FPS", ImGui::GetIO().Framerate);
+    ImGui::End();
+}
+
+void start_unit_tests()
+{
+    GbTest test(emu);
+    test.launch_tests();
 }
 
 int main()
@@ -104,21 +219,17 @@ int main()
     ImGui_ImplSDL2_InitForSDLRenderer(window, renderer);
     ImGui_ImplSDLRenderer_Init(renderer);
 
-    GbE emu;
+    emu = new GbE();
+    start_unit_tests();
+    emu->reset();
 
     size_t boot_rom_size = 0;
     auto boot_rom = load_file("C:\\Projects\\gb_emu\\resource\\dmg_rom.bin", &boot_rom_size);
-    emu.load_boot_rom(boot_rom_size, boot_rom);
+    emu->load_boot_rom(boot_rom_size, boot_rom);
 
     size_t rom_size = 0;
-    auto rom = load_file("C:\\Projects\\gb_emu\\resource\\01-special.gb", &rom_size);
-    emu.load_rom(rom_size, rom);
-
-    uint16_t breakpoints[] = {0x100};
-    bool use_breakpoints = false;
-    bool breakpoint_reached = false;
-    bool cycling_enabled = true;
-    bool stop_at_0x0100 = true;
+    auto rom = load_file("C:\\Projects\\gb_emu\\resource\\blargg\\06-ld r,r.gb", &rom_size);
+    emu->load_rom(rom_size, rom);
 
     SDL_Event e;
     bool quit = false;
@@ -140,90 +251,37 @@ int main()
         ImGui_ImplSDLRenderer_NewFrame();
         ImGui_ImplSDL2_NewFrame();
 
-        ImGui::NewFrame();
-        ImGui::Begin("FPS");
-        ImGui::Text("%.1f FPS", ImGui::GetIO().Framerate);
-        ImGui::End();
+        debugger_fps_counter();
+        debugger_main();
+        debugger_breakpoints();
 
-        ImGui::Begin("Registers");
-        ImGui::Text("PC: %#04x\n ", emu.get_PC());
+        if (cycling_enabled && !breakpoint_reached && !emu->isStopped() && !emu->isHalted()) {
+            for (int i = 0; i < cycles_per_frame; i++) {
+                emu->execute();
 
-        debuggerRegisters(&emu);
-        debuggerFlags(&emu);
-
-        if (cycling_enabled && !breakpoint_reached) {
-            for (int i = 0; i < 18000; i++) {
-                emu.execute();
-
-                if (emu.isHalted()) {
+                if (emu->isHalted()) {
                     std::cout << "Received HALT" << std::endl;
-                    breakpoint_reached = true;
                     break;
-                } else if (emu.isStopped()) {
+                } else if (emu->isStopped()) {
                     std::cout << "Received STOP" << std::endl;
-                    breakpoint_reached = true;
                     break;
                 }
 
                 if (use_breakpoints) {
-                    uint16_t pc = emu.get_PC();
+                    uint16_t pc = emu->get_PC();
 
-                    for (uint16_t addr : breakpoints) {
-                        if (addr == pc) {
+                    for (Breakpoint bp : breakpoints) {
+                        if (bp.enabled && bp.address == pc) {
                             std::cout << "Breakpoint reached at " << std::hex
-                                      << (unsigned int) addr << std::dec <<std::endl;
+                                      << (unsigned int) bp.address << std::dec <<std::endl;
                             breakpoint_reached = true;
                             break;
                         }
                     }
                 }
-
-                if (breakpoint_reached) {
-                    break;
-                }
             }
         }
 
-        if (ImGui::Button("Cycle")) {
-            emu.execute();
-            std::cout << std::hex << std::setw(4) << std::setfill('0')
-                      << (unsigned int) emu.get_last_PC() << "\t"
-                      << std::setw(2) << std::setfill('0')
-                      << (unsigned int) emu.get_last_opcode();
-
-            if (emu.get_last_opcode() == 0xCB) {
-                std::cout << (unsigned int) emu.get_last_CB_opcode();
-            }
-
-            std::cout << std::dec << std::endl;
-
-            if (emu.isHalted()) {
-                std::cout << "Received HALT" << std::endl;
-                break;
-            } else if (emu.isStopped()) {
-                std::cout << "Received STOP" << std::endl;
-                break;
-            }
-        }
-
-        if (cycling_enabled && !breakpoint_reached) {
-            if (ImGui::Button("Pause")) {
-                cycling_enabled = false;
-            }
-        } else {
-            if (ImGui::Button("Resume")) {
-                cycling_enabled = true;
-                breakpoint_reached = false;
-            }
-        }
-
-        if (ImGui::Button("Print SPI")) {
-            std::cout << "SPI buffer data:\n"
-                      << emu.get_spi_buffer_data().data() << std::endl
-                      << "SPI buffer data end" << std::endl;
-        }
-
-        ImGui::End();
         ImGui::Render();
 
         SDL_RenderClear(renderer);
